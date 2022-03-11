1825cca3fe87209cdcb39ad07e6670a1cc88ada8
Toffyro20 <98594110+Toffyro20@users.noreply.github.com>
2022-03-12 01:37:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace ASCIIconvertor
+{
+    internal class Program
+    {
+        private const double WID = 2.3;
+        private const int m_W = 200;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+           
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Images | *.png; *.jpg; *.JPEG"
+            };
+
+            
+            while (true)
+            {
+                Console.ReadLine();
+                if (openFileDialog.ShowDialog() != DialogResult.OK)
+                    continue;
+
+                Console.Clear();
+
+                var bitmap = new Bitmap(openFileDialog.FileName);
+                bitmap = ResizeBitmap(bitmap);
+                bitmap.toGray();
+
+                var converter = new conv(bitmap);
+                var rows = converter.Convert();
+
+                foreach (var row in rows)
+                    Console.WriteLine(row);
+
+                File.WriteAllLines("image.txt", rows.Select(r=> new string(r)));
+
+                Console.SetCursorPosition(0, 0);
+
+            }
+
+        }
+
+
+        private static Bitmap ResizeBitmap(Bitmap bitmap)
+        {
+            var newH = bitmap.Height / WID * m_W / bitmap.Width;
+            if (bitmap.Width > m_W || bitmap.Height > newH)
+                bitmap = new Bitmap(bitmap, new Size(m_W, (int)newH));
+            return bitmap;
+        }
+    }
+}
---
+++ conv.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace ASCIIconvertor
+{
+    public class conv
+    {
+        private readonly char[] table = { '.', ',', ':', '+', '*', '?', '%', 'S', '#', '@' };
+        private readonly Bitmap _bitmap;
+        public conv(Bitmap bitmap)
+        {
+            _bitmap = bitmap;
+        }
+        public char[][] Convert()
+        {
+            var result = new char[_bitmap.Height][];
+
+            for (int y = 0; y < _bitmap.Height; y++)
+            {
+                result[y] = new char[_bitmap.Width];
+                for (int x = 0; x < _bitmap.Width; x++)
+                {
+                    int mapIndx = (int)Map(_bitmap.GetPixel(x,y).R, 0, 255, 0, table.Length - 1 );
+                    result[y][x] = table[mapIndx];
+                }
+            }
+
+            return result;
+        }
+        private float Map(float vToMap, float st1, float sto1, float st2, float sto2)
+        {
+            return ((vToMap - st1) / (sto1 - st1)) * (sto2 - st2) + st2;
+        }
+    }
+}
